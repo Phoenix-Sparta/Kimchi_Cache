e5eb56de02f8c6f28b01eae03db153a4ac0fc03b
Peatruck <patrickmassonward@gmail.com>
2024-04-25 17:24:07 +0100

    Added further testing

--- src/main/java/com/sparta/ps/kimchi/Employee.java
+++ src/main/java/com/sparta/ps/kimchi/Employee.java
@@ -1,9 +1,23 @@
 package com.sparta.ps.kimchi;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record Employee(int empID, String prefix, String firstName, char middleInitial, String lastName,
                        char gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoin, int salary, int age) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return empID == employee.empID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(empID);
+    }
 
     @Override
     public String toString() {
